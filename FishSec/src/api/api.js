8977878a630a131635078469da477986311704a1
Aleksey <rusal@bk.ru>
2023-11-25 01:02:24 +0300

    final

--- FishSec/src/api/api.js
+++ FishSec/src/api/api.js
@@ -1,15 +1,24 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://www.virustotal.com',
+
 })
+
+
 
 export default class API {
     static async sendMessage(text) {
-        // console.log(topic_num)
-        const resp = await axios.post(`/s`, {
-            text: text,
-        })
-        return resp.data
+        // // console.log(topic_num)
+        // const resp = await axios.post('/api/v3/urls', {
+        //   headers: {
+        //     accept: 'application/json',
+        //     'content-type': 'application/x-www-form-urlencoded'
+        //   }
+        //     url: text,
+        //     'x-apikey': 'be8462184f71a44f51078d744c984b4fca7ffcf3cb294fd6b39dd6b3e8338fef',
+        // })
+
+
     }
 }
--- FishSec/src/pages/IndexPage.vue
+++ FishSec/src/pages/IndexPage.vue
@@ -6,9 +6,15 @@
       <q-input outlined v-model="text" label="Outlined" />
       </div>
       <div>
-        {{text}}
-        <q-btn color="primary" label="Отправить" />
-        {{res}}
+        <q-btn color="primary" label="Отправить" @click="onSendClick"/>
+
+        <div class="q-pa-md" style="max-width: 300px">
+        <q-input
+        v-model="res"
+        filled
+        type="textarea"
+    />
+  </div>
       </div>
     </div>
     </q-card>
@@ -18,29 +24,55 @@
 
 <script>
 import { defineComponent, ref  } from 'vue'
-import API from "../api/api";
-// export default defineComponent({
-//   name: 'IndexPage'
-// })
+import axios from 'axios';
 export default defineComponent({
   setup () {
     return {
-      text: ref('Значение по умолчанию'),
+      text: ref('ya.ru'),
       res: ref('')
     }
   },
   methods: {
     // Реакция на кнопку отправки
     async onSendClick() {
-      try {
-        this.res = await API.sendMessage(this.text);
-        console.log(this.text);
-        console.log(this.res);
-        // this.$router.push("/vis");
-      } catch (e) {
-        console.error(e);
-      }
-    }
+      var apikey = '';
+      const encodedParams = new URLSearchParams();
+        encodedParams.set('url', this.text);
+        const options = {
+          method: 'POST',
+          url: 'https://www.virustotal.com/api/v3/urls',
+          headers: {
+            accept: 'application/json',
+            'x-apikey': apikey,
+            'content-type': 'application/x-www-form-urlencoded'
+          },
+          data: encodedParams,
+        };
+        try {
+              const resp = await axios.request(options)
+              this.res = resp.data.data.id
+              console.log(this.res)
+            } catch(e) {
+              console.error(e)
+        }
+        const options2 = {
+          method: 'GET',
+          url: 'https://www.virustotal.com/api/v3/analyses/'+this.res,
+          headers: {
+            accept: 'application/json',
+            'x-apikey': apikey
+          }
+        };
+
+        try {
+              var resp = await axios.request(options2)
+              resp = resp.data.data.attributes
+              this.res = JSON.stringify(resp);
+              console.log(this.res)
+            } catch(e) {
+              console.error(e)
+        }
+        }
     },
 })
 </script>
